08ee0ea11f1a350144d3a8db9b7d7123131b4e1f
Stefan <sk893q@gmail.com>
2022-01-30 11:22:41 -0500

    ..

--- src/pages/api/verifyemail.js
+++ src/pages/api/verifyemail.js
@@ -1,7 +1,6 @@
 import { UpdateUser } from "../../util/User"
 
 export default async(req, res)=>{
-    console.log(req)
     if(req.method === "POST"){
         if(UpdateUser(req.body.email)) return res.status(200).redirect(process.env.NEXTAUTH_URL)
         return res.status(401).send("Unathorized access")
